cc9fb6b5d64997f0b7f9d0dfaa9532d806fdfda7
nguyenductri1108 <73732855+nguyenductri1108@users.noreply.github.com>
2021-03-31 20:50:18 +0700

    Add files via upload

---
+++ BT_Tuan/tuan2/lab2.1/Exercise/2-1.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+	<title>Thank You</title>
+</head>
+<body>
+	<?php
+		$name = $_POST["name"];
+		$class = $_POST["Class"];
+		$university = $_POST["University"];
+		$major = $_POST["major"];
+		$gender = $_POST["gender"];
+		print ("Hello, $name<br>");
+		print ("You are studying at $class $university<br>");
+		print ("Your major is $major<br>");
+		print ("Your gender is $gender<br>");
+		print ("Your hobbies is<br> ");
+		$a = 1;
+		if (isset($_POST['hobbie'])) {
+      		foreach($_POST['hobbie'] as $value) {
+         	//Xử lý các phần tử được chọn
+      	print ("\t $a.");
+        echo $value."<br/>";
+        $a +=1;
+       }
+}
+	?>
+</body>
+</html>

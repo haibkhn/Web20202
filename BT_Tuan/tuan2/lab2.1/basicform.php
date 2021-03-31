c20d680b32ee6afdc056b5b958a8a476e3b33d01
haibkhn <aion_2000@yahoo.com.vn>
2021-03-31 22:37:27 +0700

    Hai sua

---
+++ BT_Tuan/tuan2/lab2.1/basicform.php
@@ -0,0 +1,11 @@
+<html>
+  <head> <title> A Simple Form</title> </head>
+  <body>
+    <form action="https://www.hust.edu.vn/" method=POST>
+      Click submit to start <br>
+      <input type="text" name="Name"> <br>
+      <input type="submit" value="Click to submit">
+      <input type="reset" value="Reset">
+    </form>
+  </body>
+</html>

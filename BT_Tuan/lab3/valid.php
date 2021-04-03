1924f92a998bd7223f960d6ba46038bda2597d94
duc-pa-vn <80450316+duc-pa-vn@users.noreply.github.com>
2021-04-03 11:12:46 +0700

    Add files via upload

---
+++ BT_Tuan/lab3/valid.php
@@ -0,0 +1,44 @@
+	<form action="valid.php" method="POST">
+		<p>
+			<label for="user">username</label>
+			<input type="text" name="user" id="user">
+			<span><?php echo $user ?></span>
+</p>
+		<p>
+		<label for="email">email</label>
+		<input type="text" name="email" id="email">
+		<span><?php echo $email ?></span>
+		</p>
+		<p>
+		<label for="phone">phone</label>
+		<input type="text" name="phone" id="phone">
+		<span><?php echo $phone ?></span>
+
+		</p>
+		<button type="submit" name="submit" value="submit">submit</button>
+	</form>
+
+<?php 
+$user = $_POST["user"];
+$phone = $_POST["phone"];
+$email = $_POST["email"];
+
+
+if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+  $email = "is a email";
+} else {
+  $email = "not is a email";
+}
+
+if (preg_match('/[^0-9]/', '', $phone);) {
+  $phone = "is a phone number";
+} else {
+  $phone = "not is a phone number";
+}
+
+if (preg_match('/[^a-zA-Z]/', '', $phone);) {
+  $user = "valid";
+} else {
+  $phone = "invalid";
+}
+

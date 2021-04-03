03aa2ada7319b260396bb503a80069327a65982a
haibkhn <aion_2000@yahoo.com.vn>
2021-04-03 23:57:47 +0700

    update practice

--- BT_Tuan/tuan3/exercise/valid.php
+++ BT_Tuan/tuan3/exercise/valid.php
@@ -1,4 +1,4 @@
-	<form action="valid.php" method="POST">
+	<form >
 		<p>
 			<label for="user">username</label>
 			<input type="text" name="user" id="user">
@@ -30,13 +30,13 @@ if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
   $email = "not is a email";
 }
 
-if (preg_match('/[^0-9]/', '', $phone);) {
+if (preg_match('/[^0-9]/', '', $phone)) {
   $phone = "is a phone number";
 } else {
   $phone = "not is a phone number";
 }
 
-if (preg_match('/[^a-zA-Z]/', '', $phone);) {
+if (preg_match('/[^a-zA-Z]/', '', $phone)) {
   $user = "valid";
 } else {
   $phone = "invalid";

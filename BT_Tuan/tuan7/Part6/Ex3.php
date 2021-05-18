653da62fa3361df259a7005c153cd19ccacda160
ruller009 <81338908+ruller009@users.noreply.github.com>
2021-05-18 23:57:37 +0700

    Add files via upload

---
+++ BT_Tuan/tuan7/Part6/Ex3.php
@@ -0,0 +1,25 @@
+<?php 
+$file = "Ex3.xml"; 
+$xml = simplexml_load_file($file) or die ("Unable to load XML file!"); 
+echo $xml->sin[0] ;
+echo "<br/>"; 
+echo $xml->sin[1] ;
+echo "<br/>"; 
+echo $xml->sin[2] ; 
+echo "<br/>";
+echo $xml->sin[3] ; 
+echo "<br/>";
+echo $xml->sin[4] ; 
+echo "<br/>";
+echo $xml->sin[5] ;
+echo "<br/>"; 
+echo $xml->sin[6] ;
+echo "<br/>";
+echo "<br/>";
+echo "<br/>"; ?>
+
+<?php 
+$file = "Ex3.xml";
+$xml = simplexml_load_file($file) or die ("Unable to load XML file!"); 
+foreach ($xml->sin as $sin) { echo "$sin"; echo "<br/>"; } ?>
+
